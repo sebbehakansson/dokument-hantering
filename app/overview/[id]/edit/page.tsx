001768844bb2bf5d8c18172de910fe35ef7b36dc
sebbehakansson <sebastian.sh74@gmail.comm>
2023-10-27 12:48:59 +0200

    styling and links

--- app/overview/[id]/edit/page.tsx
+++ app/overview/[id]/edit/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Post } from "@/interfaces";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 export default function Document({ params }: { params: { id: string } }) {
@@ -26,16 +27,18 @@ export default function Document({ params }: { params: { id: string } }) {
     }).then(() => {
         getPost();
     });
+   
   } 
 
   return (
     <div className="max-w-7xl m-auto min-h-screen text-center p-10">
       {post && <>
+        <h1 className="text-2xl mb-20 font-bold">Redigera dokument</h1>
         <label htmlFor="title">Title</label>
-        <input  value={post.title} id="title" onChange={(e) => setPost({...post, title: e.target.value})} type="text" />
+        <input className="ml-10 border-solid border-2 border-black rounded-xl "  value={post.title} id="title" onChange={(e) => setPost({...post, title: e.target.value})} type="text" /> <br />
         <label htmlFor="description">Description</label>
-        <textarea id="description" value={post.description} onChange={(e) => setPost({...post, description: e.target.value})} cols={30} rows={5}></textarea>
-        <button className="px-4 py-2 bg-sky-600 text-white text-sm font-medium rounded-md ml-10" onClick={edit}>Save</button>
+        <textarea className="mt-10 border-solid border-2 border-black rounded-xl" id="description" value={post.description} onChange={(e) => setPost({...post, description: e.target.value})} cols={30} rows={5}></textarea> <br />
+        <button className="px-4 py-2 bg-sky-600 text-white text-sm font-medium rounded-md mt-10" onClick={edit} ><Link href="/overview">Save</Link></button>
       </>}
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Overview from "./overview/page"
+import Link from "next/link";
 
 export default function Home() {
   return (
   <div>
-     <h1 className="text xl font-bold text-center t">Dina 5 senaste dokument</h1>
-     <Overview />
+     <h1 className="text-xl font-bold text-center mt-10">Dina dokumenthantering!</h1>
+     <p className="text-xl text-center mt-10 ">Sidan där du ka lägga in dina dokument som du inte vet vad du ska göra med!</p>
+     <p className="text-xl text-center ">Klicka <Link className="underline hover:text-sky-600" href={"/newDoc"}>här</Link> för att börja skapa nu!</p>
   </div>
    
   )
